eff4a56d2ab1793088ba78eacff112f050287ade
zhaoguohan <zhaoguohan@troila.com>
2023-07-01 10:53:36 +0800

    【add】新增cmake文件，以及CppDesignModel工程文件

---
+++ CppDesignModel/DesginModel.cpp
@@ -0,0 +1,12 @@
+﻿// DesginModel.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "utils.h"
+
+int main()
+{
+    CallFunc();
+
+    CallFunc2();
+    
+}
---
+++ CppDesignModel/utils.h
@@ -0,0 +1,12 @@
+#ifndef _DESGINED_MODEL_UTILS_HEADER_
+#define _DESGINED_MODEL_UTILS_HEADER_
+
+#include "CommonHead.h"
+#include "TempalateModel.h"
+
+#endif // !_DESGINED_MODEL_UTILS_HEADER_
+
+
+
+
+
