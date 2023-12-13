7dc063f6faa155dcd5e6d75e5bb6e5e678d2f003
zhaoguohan <zhaoguohan@troila.com>
2023-12-13 22:59:05 +0800

    111

--- CommonTools/AssistCollectProc/AssistCollectProc.cpp
+++ CommonTools/AssistCollectProc/AssistCollectProc.cpp
@@ -5,10 +5,10 @@ std::vector<HWND> g_top_level_wnds;
 AssistCollectProc::AssistCollectProc():
     proc_id_vec_(),
     proc_work_thread_(nullptr),
-    all_proc_info_(nullptr),
     exit_event_(NULL),
     initialized_(false)
 {
+    all_proc_info_ = std::make_shared<std::map<std::string, ROCESSINFOA>>();
 }
 
 AssistCollectProc::~AssistCollectProc()
@@ -40,7 +40,6 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
 void AssistCollectProc::GetProcIdByTopWnd()
 {
     EnumWindows(EnumWindowsProc, NULL);
-    LOGGER_INFO("top_level_wnds_ size: {}", g_top_level_wnds.size());
 
     for (HWND hwnd : g_top_level_wnds) 
     {
@@ -61,8 +60,7 @@ void AssistCollectProc::GetProcIdByTopWnd()
 
 void AssistCollectProc::GetProcInfoByPid(DWORD processId, 
                                         std::string & desc_str, 
-                                        std::string & version_str,
-                                        std::chrono::milliseconds & duration)
+                                        std::string & version_str)
 {
     HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
     if (hProcess != NULL) 
@@ -81,14 +79,8 @@ void AssistCollectProc::GetProcInfoByPid(DWORD processId,
             {
                 desc_str = ANSIToUTF8(desc_str);
             }
-            if(!QueryValue("ProductVersion", processPath, version_str))
-            {
-                LOGGER_ERROR("QueryValue processId:{} ProductVersion failed!",processId);
-            }
-            if(!GetProcRunTimeByHandle(hProcess, duration))
-            {
-                LOGGER_ERROR("GetProcRunTimeByHandle ");
-            }
+            (void)QueryValue("ProductVersion", processPath, version_str);
+
         }
         else
         {
@@ -105,41 +97,6 @@ void AssistCollectProc::GetProcInfoByPid(DWORD processId,
     {
         LOGGER_ERROR("OpenProcess error: {}", GetLastError());
     }
-}
-
-BOOL AssistCollectProc::GetProcRunTimeByHandle(HANDLE hProcess, std::chrono::milliseconds & duration)
-{
-    BOOL bRet = FALSE;
-    do 
-    { 
-        if (hProcess != NULL) 
-        {
-            FILETIME createTime, exitTime, kernelTime, userTime;
-
-            if (GetProcessTimes(hProcess, &createTime, &exitTime, &kernelTime, &userTime)) {
-                FILETIME now;
-                (void)GetSystemTimeAsFileTime(&now);
-
-                ULONGLONG createTime64 = ((ULONGLONG)createTime.dwHighDateTime << 32) + createTime.dwLowDateTime;
-                ULONGLONG now64 = ((ULONGLONG)now.dwHighDateTime << 32) + now.dwLowDateTime;
-                ULONGLONG elapsedTime64 = now64 - createTime64;
-
-                ULONGLONG milliseconds = elapsedTime64 / 10000;
-
-                std::chrono::milliseconds duration_tmp(milliseconds);
-                duration = duration_tmp;
-            } 
-            else 
-            {
-                LOGGER_ERROR("Failed to get process times. Error code: {}", GetLastError());
-                break;
-            }
-        }
-        bRet = TRUE;
-    }
-    while(FALSE);
-
-    return bRet;
 }
 
 BOOL AssistCollectProc::QueryValue(const std::string &ValueName, 
@@ -205,17 +162,14 @@ BOOL AssistCollectProc::QueryValue(const std::string &ValueName,
         }
         sprintf_s(tmpstr, 256, "\\StringFileInfo\\%08lx\\%s", m_dwLangCharset, ValueName.c_str());
         LPVOID lpData;
- 
+       
         // 调用此函数查询前需要先依次调用函数GetFileVersionInfoSize和GetFileVersionInfo
         if (::VerQueryValueA((void *)m_lpVersionData, tmpstr, &lpData, &nQuerySize))
         {
             RetStr = (char*)lpData;
-        }else
-        {
-            LOGGER_ERROR("VerQueryValueA szModuleName:{} error: {}", szModuleName.c_str(), GetLastError());
-            break;
         }
-            
+        LOGGER_INFO("szModuleName: {} tmpstr:{} RetStr:{} U_RetStr:{}",  szModuleName.c_str(),tmpstr,RetStr.c_str(), ANSIToUTF8(RetStr));
+        
         bSuccess = TRUE;
     } while (FALSE);
 
@@ -241,30 +195,17 @@ std::shared_ptr<std::map<std::string, ROCESSINFOA>> AssistCollectProc::GetProcIn
 
     std::string  proc_desc;
     std::string  proc_version;
-    std::chrono::milliseconds duration;
 
     for (auto pid:proc_id_vec_)
     {
-        (void)GetProcInfoByPid(pid, proc_desc, proc_version, duration);
+        (void)GetProcInfoByPid(pid, proc_desc, proc_version);
 
         if (proc_desc.size() > 0)
         {
             ROCESSINFOA proc_info;
-            proc_info.proc_running_time = duration;
+            proc_info.proc_run_time_miniutes = 0;
             proc_info.proc_version = proc_version;
-            // 多个同名的应用，则取运行时间最早的那个应用的运行时间
-            if (proc_info_vec->find(proc_desc) != proc_info_vec->end())
-            {
-                if (proc_info_vec->at(proc_desc).proc_running_time < duration)
-                {
-                    proc_info_vec->at(proc_desc).proc_running_time = duration;
-                }
-                
-            }
-            else
-            {
-                proc_info_vec->insert(std::make_pair(proc_desc, proc_info));
-            }
+            proc_info_vec->insert(std::make_pair(proc_desc, proc_info));
         }
     }
     return proc_info_vec;
@@ -300,38 +241,18 @@ void AssistCollectProc::WorkThread()
     {
         DWORD count = 0;
 
-        while (count++ < 15)  // 计时15分钟
+        while (count++ < 1)  // 计时15分钟
         {
-            DWORD dwRet = WaitForSingleObject(exit_event_,  60 *  1000);  // 计时1分钟
+            DWORD dwRet = WaitForSingleObject(exit_event_, 10 * 1000);  // 计时1分钟
             if (WAIT_OBJECT_0 == dwRet)
             {
                 return; // 退出线程
             }
             else if (WAIT_TIMEOUT == dwRet) // 不在all_proc_info_中，则新增该应用的记录；在all_proc_info_中，则运行时间加一分钟
             {
-                // 1. 获取进程信息
+                // 采集应用信息
                 std::shared_ptr<std::map<std::string, ROCESSINFOA>> proc_info_vec = GetProcInfo();
-                if (proc_info_vec->size() > 0)
-                {
-                    if (all_proc_info_ == nullptr)
-                    {
-                        all_proc_info_ = proc_info_vec;
-                    }
-                    else
-                    {
-                        for (auto it = proc_info_vec->begin(); it != proc_info_vec->end(); ++it)
-                        {
-                            if (all_proc_info_->find(it->first) != all_proc_info_->end())
-                            {
-                                all_proc_info_->at(it->first).proc_running_time += std::chrono::minutes(1);
-                            }
-                            else
-                            {
-                                all_proc_info_->insert(std::make_pair(it->first, it->second));
-                            }
-                        }
-                    }
-                }
+                (void)SaveProcInfo(proc_info_vec);
             }
             else
             {
@@ -340,50 +261,16 @@ void AssistCollectProc::WorkThread()
             }
         }
 
-        // 1. 转换时间的格式
-        std::shared_ptr<std::map<std::string, ROCESSINFOA>> send_proc_data = all_proc_info_;
-        for (auto it = all_proc_info_->begin(); it != all_proc_info_->end(); ++it)
+        // 将收集数据转换成json格式字符串
+        std::string app_info_json = ConvertProcInfoToJson();
+        if (app_info_json.size() <= 0)
         {
-            std::chrono::milliseconds duration = it->second.proc_running_time;
-            std::chrono::hours hours = std::chrono::duration_cast<std::chrono::hours>(duration);
-            duration -= std::chrono::duration_cast<std::chrono::milliseconds>(hours);
-            std::chrono::minutes minutes = std::chrono::duration_cast<std::chrono::minutes>(duration);
-            duration -= std::chrono::duration_cast<std::chrono::milliseconds>(minutes);
-            std::chrono::seconds seconds = std::chrono::duration_cast<std::chrono::seconds>(duration);
-
-            std::stringstream ss;
-            if (hours.count() > 0) {
-                ss << hours.count() << ":";
-            }
-            if (minutes.count() >= 0) {
-                ss << minutes.count() << ":";
-            }
-            if (seconds.count() >= 0 || (hours.count() == 0 && minutes.count() == 0)) {
-                ss << seconds.count() << ":";
-            }
-            it->second.proc_string_runtime = ss.str();
+            LOGGER_ERROR("ConvertProcInfoToJson error");
+            continue;
         }
-
-        // 2. 将收集数据转换成json格式字符串
-        nlohmann::json send_proc_data_json;
-        send_proc_data_json["VMuuid"] = "1234567890-2-2--2222";
-        
-         nlohmann::json appDataArray = nlohmann::json::array();
-         
-        for (auto it = all_proc_info_->begin(); it != all_proc_info_->end(); it++)
-        {
-            nlohmann::json appData;
-            appData["AppName"] = it->first;
-            appData["Version"] = it->second.proc_version;
-            appData["RunningTime"] = it->second.proc_string_runtime;
-
-            appDataArray.push_back(appData);
-        }
-        
-        send_proc_data_json["AppData"] = appDataArray;
-
-        LOGGER_INFO("app_info_json: {}", send_proc_data_json.dump());
-        // 3. 上传数据给平台
+        LOGGER_INFO("app_info_json: {}", app_info_json);
+        LOGGER_INFO("---------------------------");
+        // 上传数据给平台
     }
 }
 
@@ -407,4 +294,100 @@ BOOL AssistCollectProc::Initialize()
     } while (FALSE);
 
     return bRet;
+}
+
+
+void AssistCollectProc::SaveProcInfo(std::shared_ptr<std::map<std::string, ROCESSINFOA>> & proc_info_vec)
+{
+    // 将每分钟采集到的数据，保存到15分钟内采集到的数据中
+    if (proc_info_vec->size() > 0)
+    {
+        if (all_proc_info_ == nullptr)  // 第一次采集数据
+        {
+            all_proc_info_ = proc_info_vec;
+        }
+        else
+        {
+            for (auto it = proc_info_vec->begin(); it != proc_info_vec->end(); ++it) // 遍历每分钟采集到的数据
+            {
+                if (all_proc_info_->find(it->first) != all_proc_info_->end())
+                {
+                    // 应用在all_proc_info_中，则运行时间加一分钟
+                    all_proc_info_->at(it->first).proc_run_time_miniutes += 1;
+                }
+                else
+                {
+                    // 应用不在all_proc_info_中，则新增该应用的记录
+                    all_proc_info_->insert(std::make_pair(it->first, it->second));
+                }
+            }
+        }
+    }
+}
+
+std::string AssistCollectProc::ConvertProcInfoToJson()
+{
+    nlohmann::json send_proc_data_json;
+    send_proc_data_json["VMuuid"] = GetVmUuid();
+    
+    nlohmann::json appDataArray = nlohmann::json::array();
+
+    for (auto it = all_proc_info_->begin(); it != all_proc_info_->end(); it++)
+    {
+        nlohmann::json appData;
+        appData["AppName"] = it->first;
+        appData["Version"] = it->second.proc_version;
+        appData["RunningTime"] = it->second.proc_run_time_miniutes;
+
+        appDataArray.push_back(appData);
+    }
+    
+    send_proc_data_json["AppData"] = appDataArray;
+
+    return send_proc_data_json.dump();
+}
+
+std::string AssistCollectProc::GetVmUuid()
+{
+    std::string retStr;
+    HKEY hKey = NULL;
+    DWORD dwType = REG_SZ;
+    DWORD dwSize = 0;
+
+    do {
+        if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SOFTWARE\\TroilaKly\\FeatureAssist", 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
+            if (RegQueryValueExA(hKey, "vmUuid", NULL, &dwType, NULL, &dwSize) == ERROR_SUCCESS) {
+                char* buffer = new char[dwSize];
+                if (buffer == NULL) {
+                    LOGGER_ERROR("new buffer failed!");
+                    break;
+                }
+
+                if (RegQueryValueExA(hKey, "vmUuid", NULL, NULL, reinterpret_cast<LPBYTE>(buffer), &dwSize) == ERROR_SUCCESS) {
+                    retStr = buffer;
+                }
+                else {
+                    LOGGER_ERROR("RegQueryValueExA failed! errcode : {}", GetLastError());
+                }
+                if (buffer) {
+                    delete[] buffer;
+                    buffer = NULL;
+                }
+            }
+            else {
+                LOGGER_ERROR("RegQueryValueExA sting size failed! errcode : {}", GetLastError());
+            }
+        }
+        else {
+            LOGGER_ERROR("RegOpenKeyExA key failed! errcode : {}", GetLastError());
+        }
+    } while (FALSE);
+    
+    if (hKey)
+    {
+        RegCloseKey(hKey);
+        hKey = NULL;
+    }
+
+    return retStr;
 }
--- CommonTools/AssistCollectProc/AssistCollectProc.h
+++ CommonTools/AssistCollectProc/AssistCollectProc.h
@@ -22,8 +22,7 @@ typedef struct LANGANDCODEPAGE {
 typedef struct PROCESSINFO
 {
     std::string proc_version;
-    std::chrono::milliseconds proc_running_time;
-    std::string  proc_string_runtime;
+    int proc_run_time_miniutes;
 }ROCESSINFOA, *PROCESSINFOA;
 
 
@@ -33,18 +32,25 @@ public:
     AssistCollectProc();
     ~AssistCollectProc();
 
+    // 运行工作线程; 
+    void Run();
+    
+    // 停止工作线程;
+    void Stop();
+
+    // 初始化
+    BOOL Initialize();
+
+    //1. 每15分钟上报一次收集到的软件信息  2. 每分钟收集一次软件信息
+    void WorkThread();
+
     //枚举窗口获取进程PID
     void GetProcIdByTopWnd();
     
     //通过PID获取进程描述信息，版本信息，运行时间
     void GetProcInfoByPid(DWORD processId,
                           std::string & desc_str, 
-                          std::string & version_str,
-                          std::chrono::milliseconds & duration);
-
-    //通过进程句柄获取进程运行时间
-    BOOL GetProcRunTimeByHandle(HANDLE hProcess, 
-                                std::chrono::milliseconds & duration);
+                          std::string & version_str);
 
     //根据进程二进制所在路径，获取指定属性(版本号，描述信息)
     BOOL QueryValue(const std::string& ValueName, 
@@ -54,21 +60,18 @@ public:
     //查询任务管理器，进程选项卡中，应用的信息
     std::shared_ptr<std::map<std::string, ROCESSINFOA>> GetProcInfo();
 
-    // 运行工作线程; 
-    void Run();
-    
-    // 停止工作线程;
-    void Stop();
-
-    //1. 每15分钟上报一次收集到的软件信息  2. 每分钟收集一次软件信息
-    void WorkThread();
-
     // // 把json数据上传给平台
     // BOOL SendProcInfoToPlat();
 
-    // 初始化
-    BOOL Initialize();
+    // 将每分钟采集到的数据，保存到15分钟内采集到的数据中
+    void SaveProcInfo(std::shared_ptr<std::map<std::string, ROCESSINFOA>> & proc_info_vec);
 
+    // 将收集到的数据，转换为json字符串格式
+    std::string ConvertProcInfoToJson();
+
+    // 获取虚拟机uuid
+    std::string GetVmUuid();
+    
     //保存每次枚举窗口获取的PID
     std::vector<DWORD> proc_id_vec_;
 
