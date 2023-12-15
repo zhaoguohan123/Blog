e0b2aa886077d3ac6826b4cabdc5cb2e533c549b
zhaoguohan <zhaoguohan@troila.com>
2023-12-15 18:08:22 +0800

    11111

---
+++ MyDebugTools/InputHooks/InputHooks/InputHooks/InputHooksDlg.cpp
@@ -0,0 +1,185 @@
+﻿
+// InputHooksDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "InputHooks.h"
+#include "InputHooksDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CInputHooksDlg 对话框
+
+
+
+CInputHooksDlg::CInputHooksDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_INPUTHOOKS_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CInputHooksDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, m_list_user_kb_ctrl);
+}
+
+BEGIN_MESSAGE_MAP(CInputHooksDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(ID_OPEN_HOOK, &CInputHooksDlg::OnBnClickedOpenHook)
+	ON_BN_CLICKED(ID_CLOSE_HOOK, &CInputHooksDlg::OnBnClickedCloseHook)
+END_MESSAGE_MAP()
+
+
+// CInputHooksDlg 消息处理程序
+
+BOOL CInputHooksDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	/// R3键盘HOOK
+	m_list_user_kb_ctrl.SetExtendedStyle(LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES);
+
+	m_list_user_kb_ctrl.InsertColumn(0, _T(""), LVCFMT_LEFT, 100);
+
+	user_kb_hook_ = std::make_shared<UserKbHook>(m_list_user_kb_ctrl);
+	//for (int i = 0; i < 1000; ++i) {
+	//	CString str;
+	//	Sleep(100);
+	//	str.Format(_T("Row %d"), i + 1);
+	//	m_list_user_kb_ctrl.InsertItem(i, str);
+	//}
+
+	/// 
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CInputHooksDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CInputHooksDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CInputHooksDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CInputHooksDlg::OnBnClickedOpenHook()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	user_kb_hook_->SetHook();
+}
+
+
+void CInputHooksDlg::OnBnClickedCloseHook()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	user_kb_hook_->UnSetHook();
+}
---
+++ MyDebugTools/InputHooks/InputHooks/InputHooks/InputHooksDlg.h
@@ -0,0 +1,40 @@
+﻿
+// InputHooksDlg.h: 头文件
+//
+
+#pragma once
+#include "UserKbHook/UserKbHook.h"
+#include <memory>
+
+// CInputHooksDlg 对话框
+class CInputHooksDlg : public CDialogEx
+{
+// 构造
+public:
+	CInputHooksDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_INPUTHOOKS_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOpenHook();
+	afx_msg void OnBnClickedCloseHook();
+	CListCtrl m_list_user_kb_ctrl;
+	std::shared_ptr<UserKbHook> user_kb_hook_;
+};
